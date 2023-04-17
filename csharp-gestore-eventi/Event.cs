76638af697508eb4eb9fa1a090e78c51bb8cc001
AlessandroDiNardo <alexdna003@gmail.com>
2023-04-17 12:23:54 +0200

    fixes

--- csharp-gestore-eventi/Event.cs
+++ csharp-gestore-eventi/Event.cs
@@ -8,49 +8,52 @@ namespace csharp_gestore_eventi
 {
     internal class Event
     {
-        private string? title;
-        private DateTime date;
-        private int capacity;
-        private int booked_seats;
+        private string? _title;
+        private DateTime _date;
+        private int _capacity;
+        private int _booked_seats;
 
         //metodo per il titolo, controlla che il titolo non sia un campo vuoto o null
         public string Title
         {
-            get { return title; }
+            get { return _title; }
             set
             {
                 if (string.IsNullOrEmpty(value))
                 {
                     throw new ArgumentException("Titolo errato, il titolo non può essere un campo VUOTO!");
                 }
-                title = value;
+                _title = value;
             }
         }
 
         //metodo per la data, controlla che la data non sia un campo con un data passata
         public DateTime Date
         {
-            get { return date; }
+            get { return _date; }
             set
             {
                 if (value == DateTime.Now.Date)
                 {
                     throw new ArgumentException("Data errata, non può essere un dato passato");
                 }
-                date = value;
+                _date = value;
             }
         }
 
-        //metodo per la capeinza massisa solo in lettura
-        public int Capacity { get; private set; }
-
         //metodo per i posti occupati solo in lettura
-        public int BookedSeats { get; private set; }
+        public int BookedSeats 
+        {
+            get
+            {
+                return _booked_seats;
+            }
+        }
 
         //metodo per il calcolo dei posti posti liberi, la capienza massima - i posti occupati
         public int NumFreeSeats
         {
-            get { return capacity - booked_seats; }
+            get { return _capacity - _booked_seats; }
         }
 
         //dichiarazione costruttore, ha come parametri il titolo, la data, posti a disposizione
@@ -62,32 +65,34 @@ namespace csharp_gestore_eventi
             {
                 throw new ArgumentException("Il valore della capienza massima deve essere un dato positivo");
             }
-            Capacity = capacity;
-            booked_seats = 0; //inizializzo la variabile dei posti prenotati a 0
+
+            if (Date < DateTime.Now.Date) //controlla se la data inserita sia quella di oggi e che non sia passata
+            {
+                throw new ArgumentException($"Data inserita non corretta, hai inserito una data passata: {Date}");
+            }
+            _capacity = capacity;
+            _booked_seats = 0; //inizializzo la variabile dei posti prenotati a 0
         }
 
         // Metodo per prenotare dei posti, ha come parametro la variabile che contiene il numero di posti inseriti dell'utente
-        public void ReserveSeats(int numSeats)
+        public void ReserveSeats(int num)
         {
             if ( NumFreeSeats == 0) //controlla se ci sono di posti disponibili 
             {
                 throw new ArgumentException("Non ci sono posti disponibili per l'evento, mi spiace!");
             }
-            else if( numSeats > NumFreeSeats) //controlla se il numero di posti inseriti sia maggiore dei posti disponibili, se è vero appare messaggio di avviso
+            else if( num > NumFreeSeats) //controlla se il numero di posti inseriti sia maggiore dei posti disponibili, se è vero appare messaggio di avviso
             {
                 throw new ArgumentException($"Non ci sono tutti questi posti disponibili, posti disponibili: {NumFreeSeats}");
             }
-            else if( Date != DateTime.Now.Date) //controlla se la data inserita sia quella di oggi e che non sia passata
-            {
-                throw new ArgumentException($"Data inserita non corretta, hai inserito una data passata: {Date}");
-            }
-            booked_seats += numSeats; //aumenta il numero di posti prenotati
+          
+            _booked_seats += num; //aumenta il numero di posti prenotati
         }
 
         //metodo per la cancellazione di posti prenotati, ha come parametro la variabile che contiene il numero di posti inseriti dell'utente
-        public void CancelSeats(int numSeats)
+        public void CancelSeats(int num)
         {
-            if( numSeats > booked_seats) //controlla se il numero inserito è maggiore dei posti disponibili appare messaggio di avviso
+            if( num > _booked_seats) //controlla se il numero inserito è maggiore dei posti disponibili appare messaggio di avviso
             {
                 throw new ArgumentException("Non ci sono abbastanza posti prenotati da disdire");
             }
@@ -95,15 +100,13 @@ namespace csharp_gestore_eventi
             {
                 throw new ArgumentException($"Data inserita non corretta, hai inserito una data passata: {Date}");
             }
-            booked_seats -= numSeats;
+            _booked_seats -= num;
         }
 
         //metodo che restituisce una stringa contenente: data formattata – titolo
         public override string ToString()
         {
-            return date.ToString($"dd/MM/yyyy - {title}");
+            return _date.ToString($"dd/MM/yyyy - {_title}");
         }
-
- 
     }
 }
--- csharp-gestore-eventi/Program.cs
+++ csharp-gestore-eventi/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
+            //chiediamo all'utente di inserire i dati per un nuovo evento
+            Console.Write("Inserisci titolo dell'evento: ");
+            string title = Console.ReadLine();
 
+            //chiediamo all'utente di inserire la data dell'evento
+            Console.Write("Inserisci la data dell'evento (in formato: dd/MM/yyyy): ");
+            DateTime date = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+
+            //chiediamo all'utente di inserire la capienza massima dell'evento
+            Console.Write("Inserisci la capienza massima dell'evento: ");
+            int capacity;
+            while (!int.TryParse(Console.ReadLine(), out capacity))
+                Console.Write("Inserisci un numero");
+
+            Event newEvent = new Event(title, date, capacity);
+            Console.WriteLine(newEvent.ToString());
+            Console.WriteLine("Il tuo evento è stato creato!");
+
+            //chiediamo all'utente di inserire il numero di posti da prenotare
+            Console.Write("Vuoi prenotare dei posti? (si/no) : ");
+            string answer = Console.ReadLine();
+
+            while (answer.ToLower() == "si")
+            {
+                Console.Write("Inserisci il numero di posti da prenotare:");
+                int numSeats = int.Parse(Console.ReadLine());
+
+                try
+                {
+                    newEvent.ReserveSeats(numSeats);
+                    Console.WriteLine("Prenotazione effettuata con successo");
+                    Console.WriteLine($"Posti prenotati: {newEvent.BookedSeats}, posti disponibili: {newEvent.NumFreeSeats}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                Console.WriteLine("Vuoi prenotare altri posti? (si/no)");
+                answer = Console.ReadLine();
+            }
         }
     }
 }
